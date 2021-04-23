f569ad855f51c472e0f7194ae067c48d0641f4e8
comeheredart <wonee0321@gmail.com>
2021-04-23 22:26:48 +0900

    [ADD] 2주차 과제 완성

--- assignment/kakaotalk/kakaotalk/FriendsListViewController.swift
+++ assignment/kakaotalk/kakaotalk/FriendsListViewController.swift
@@ -12,18 +12,14 @@ class FriendsListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func profileImageClicked(_ sender: Any) {
+        guard let profileVC = self.storyboard?.instantiateViewController(withIdentifier: "ProfileViewController") else { return }
+        
+        profileVC.modalPresentationStyle = .fullScreen
+        self.present(profileVC, animated: true, completion: nil)
     }
-    */
-
+    
 }
---
+++ assignment/kakaotalk/kakaotalk/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  kakaotalk
+//
+//  Created by JEN Lee on 2021/04/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func xBtnClicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+}
