7df4d73a06e1d6a827e0c20cb29bb2a7ced478da
comeheredart <wonee0321@gmail.com>
2021-04-23 18:04:44 +0900

    [ADD] 2주차 과제 중간완성

---
+++ assignment/kakaotalk/kakaotalk/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  kakaotalk
+//
+//  Created by JEN Lee on 2021/04/23.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
